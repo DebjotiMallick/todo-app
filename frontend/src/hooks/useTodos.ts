5bd143df098ede137b048d7202ed99678fcc6d3b
debjotimallick <debjoti97@gmail.com>
2025-06-24 12:53:45 +0530

    change get todos

--- frontend/src/hooks/useTodos.ts
+++ frontend/src/hooks/useTodos.ts
@@ -1,9 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
-import axiosInstance from "../api/axiosInstance";
+// import axiosInstance from "../api/axiosInstance";
+
+// const fetchTodos = async () => {
+//   const response = await axiosInstance.get("/todos");
+//   return response.data;
+// };
 
 const fetchTodos = async () => {
-  const response = await axiosInstance.get("/todos");
-  return response.data;
+  try {
+    const response = await fetch(
+      "https://todo.debjotimallick.store/api/todos/"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data; // return so it's usable
+  } catch (error) {
+    console.error("Failed to fetch todos:", error);
+    throw error; // propagate the error to React Query or caller
+  }
 };
 
 export const useTodos = () => {
